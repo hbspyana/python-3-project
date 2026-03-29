d815b881522035f86da3231d7810f8cf15018eea
hbspyana <tatiananamoco@gmail.com>
2026-03-29 18:42:55 +0200

    update main

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from fastapi.staticfiles import StaticFiles
 from app.routers import users, tasks
 from app.models.models import Base
 from sqlalchemy import create_engine
+import os
 
 engine = create_engine('sqlite:///./attendance.db')
 Base.metadata.create_all(bind=engine)
@@ -13,10 +14,12 @@ app.include_router(users.router, prefix='/users')
 app.include_router(tasks.router, prefix='/tasks')
 app.mount('/static', StaticFiles(directory='.'), name='static')
 
-# @app.get('/')
-# def root():
-#     return {'message': 'Attendance API running'}
+@app.get('/')
+def root():
+    file_path = os.path.join(os.getcwd(), 'index.html')
+    return FileResponse(file_path)
 
 @app.get('/dashboard')
 def dashboard():
-    return FileResponse('index.html')
+    file_path = os.path.join(os.getcwd(), 'index.html')
+    return FileResponse(file_path)
