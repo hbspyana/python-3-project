231ae081c68343b6d4746bff51bc67945ce0be56
hbspyana <tatiananamoco@gmail.com>
2026-03-29 17:59:52 +0200

    update main

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 from app.routers import users, tasks
 from app.models.models import Base
 from sqlalchemy import create_engine
@@ -9,7 +11,12 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 app.include_router(users.router, prefix='/users')
 app.include_router(tasks.router, prefix='/tasks')
+app.mount('/static', StaticFiles(directory='.'), name='static')
 
 @app.get('/')
 def root():
     return {'message': 'Attendance API running'}
+
+@app.get('/dashboard')
+def dashboard():
+    return FileResponse('index.html')
