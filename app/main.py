529f3fe616f2bf9c92105b36ea8f24565cac8b3b
hbspyana <tatiananamoco@gmail.com>
2026-03-31 23:47:57 +0200

    update

--- app/main.py
+++ app/main.py
@@ -1,10 +1,7 @@
 from fastapi import FastAPI
-from fastapi.responses import FileResponse
-from fastapi.staticfiles import StaticFiles
 from app.routers import users, tasks
 from app.models.models import Base
 from sqlalchemy import create_engine
-import os
 
 engine = create_engine('sqlite:///./attendance.db')
 Base.metadata.create_all(bind=engine)
@@ -12,7 +9,6 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 app.include_router(users.router, prefix='/users')
 app.include_router(tasks.router, prefix='/tasks')
-app.mount('/static', StaticFiles(directory='.'), name='static')
 
 @app.get('/')
 def root():
